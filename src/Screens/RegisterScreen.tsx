03020769dd8137af00edec99c1957979248d90f7
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-01-02 20:49:47 -0500

    Finalizado el diseño de registro

--- src/Screens/RegisterScreen.tsx
+++ src/Screens/RegisterScreen.tsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native'
-import { Text, TextInput } from 'react-native-paper'
+import { Button, Text, TextInput } from 'react-native-paper'
 import { styles } from '../Theme/Styles'
 
+interface FormRegister{
+    email:string;
+    password: string;
+}
+
 export const RegisterScreen = () => {
+    const [formRegister, setFormRegister] = useState<FormRegister>({
+        email: '',
+        password:''
+    })
+
+const handleSetValues=(key:string, value:string)=>{
+    setFormRegister({... formRegister,[key]:value})
+}
+
     return (
         <View style={styles.root}>
-            <Text>Registrarse</Text>
+            <Text variant="headlineMedium">Registrarse</Text>
             <TextInput
                 label="Email"
                 mode='outlined'
                 placeholder='Ingrese su correo'
+                selectionColor='red'
                 style={styles.input}
             />
             <TextInput
                 label="Contraseña"
                 mode='outlined'
                 placeholder='Ingrese su contraseña'
+                secureTextEntry={true}
                 style={styles.input}
             />
+            <Button style={styles.button}
+                icon="account-circle-outline" 
+                mode="contained" 
+                buttonColor='#153E90'
+                onPress={() => console.log('Pressed')}>
+                Registrate
+            </Button>
         </View>
     )
 }
--- src/Theme/Styles.tsx
+++ src/Theme/Styles.tsx
@@ -4,12 +4,17 @@ export const styles = StyleSheet.create({
     root: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'lightblue', 
+        alignItems: 'center', 
+        gap: 4
     },
     
     input: {
         width: '80%'
+    },
+
+    button:{
+        width: '80%',
+        marginTop: 10
     }
 
 })
